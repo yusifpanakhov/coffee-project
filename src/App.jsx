639752c9980ed89a0438d7e84c070367276d380e
Yusif Panahov <yusifpanahov@gmail.com>
2025-12-11 21:48:28 +0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Footer from "./pages/Footer";
+import Header from "./pages/Header";
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
-      <Footer />
-    </div>
+    <Header/>
+    <Footer />
+</div>
   )
 }
 
